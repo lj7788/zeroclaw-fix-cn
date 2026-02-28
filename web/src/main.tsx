85f20efaeb31f9edd3a7dd24c173fbbf92efda65
lj7788 <lj7788@126.com>
2026-02-28 18:03:31 +0800

    refactor(web): 将路由从 BrowserRouter 改为 HashRouter
    使用 HashRouter 替代 BrowserRouter，这样可以在不配置服务器的情况下支持 SPA 路由。
    URL 将从 /path 变为 /#/path 格式。

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Vite base '/_app/' scopes static asset URLs only; app routes stay rooted at '/' for SPA fallback. */}
-    <BrowserRouter basename="/">
+    {/* Using HashRouter for SPA routing without server configuration */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
